f434ad4248d6d93c18791c855d6ae2c599e34118
harsh160605 <harshvardhanchavan713@gmail.com>
2026-03-02 17:52:16 +0530

    bubble sort

--- src/sorting.java
+++ src/sorting.java
@@ -1,6 +1,5 @@
-public class sorting {
-    public static void main(String[] args){
-        int [] arr = {1,8,3,4,5};
+class sorts{
+    public void SelectionSort(int[] arr){
         int a = arr.length;
         for(int i = 0;i<=a-1; i++){
             int temp = i;
@@ -18,6 +17,30 @@ public class sorting {
         for (int num: arr){
             System.out.println(num);
         }
+    }
+
+    public void bubbleSor(int[] arr){
+        int n= arr.length;
+        for(int i = n-1;i>=0;i--){
+            for(int j = 0; j<= i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] =arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+public class sorting {
+    public static void main(String[] args){
+        int [] arr = {1,8,3,4,5};
+        sorts obj = new sorts();
+        obj.bubbleSor(arr);
 
     }
 }
