5e444765b6a7a7764fd0ea86ec36606b40292517
harsh160605 <harshvardhanchavan713@gmail.com>
2026-03-04 19:01:44 +0530

    merge sort

--- src/sorting.java
+++ src/sorting.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class sorts{
     public void SelectionSort(int[] arr){
         int a = arr.length;
@@ -34,13 +35,76 @@ class sorts{
             System.out.print(num + " ");
         }
     }
+
+    public void insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i <= n-1; i++){
+            for(int j = 0; j<= n-1; j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                }
+            }
+        }
+    }
+
+
+    public void merge(int[] arr, int low, int mid, int high) {
+
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid + 1;
+
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.add(arr[left++]);
+            else
+                temp.add(arr[right++]);
+        }
+
+
+        while (left <= mid)
+            temp.add(arr[left++]);
+
+
+        while (right <= high)
+            temp.add(arr[right++]);
+
+        for (int i = low; i <= high; i++)
+            arr[i] = temp.get(i - low);
+    }
+
+    public void mergeSort(int[] arr,int low,int high ){
+        if(low>=high)
+            return;
+
+        int mid = (low + high)/2;
+
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr, low, mid, high);
+
+    }
 }
+
+
 
 public class sorting {
     public static void main(String[] args){
         int [] arr = {1,8,3,4,5};
         sorts obj = new sorts();
-        obj.bubbleSor(arr);
+//        obj.bubbleSor(arr);
+        obj.mergeSort(arr, 0, arr.length - 1);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
 
     }
 }
+
+
+
+
+
